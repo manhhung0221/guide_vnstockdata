968a50392c62f64cfe9ccfed4faa9f0cef8dcd8d
Hoang Manh Hung <hung.hm@vdsc.com.vn>
2024-10-08 15:10:57 +0700

    update version 1.0.19

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'stockvip'
 copyright = '2024, stockvip'
 author = 'stockvip'
-release = '1.0.19'
+release = '1.0.19-beta'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
